ee0f0bbaf3ac5badd970244683aee941c1af9022
James Evans <106929700+broken-helix@users.noreply.github.com>
2024-09-30 02:07:05 +0100

    Change Navbar import to NavBar to match source

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/NavBar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
